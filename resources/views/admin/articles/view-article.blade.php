624ba207aaaa16e74483b028aefcf19bfa90d21c
Nay Aung Lin <nayaung54545454@gmail.com>
2023-03-06 17:00:02 +0630

    test ads implemntation on article view

--- resources/views/admin/articles/view-article.blade.php
+++ resources/views/admin/articles/view-article.blade.php
@@ -1,16 +1,37 @@
 @extends('layout.master')
-@section('meta-title',  "$article->title- NightKite")
+@section('meta-title', "$article->title- NightKite")
 @section('meta-description', "$article->meta_description")
 @section('meta-canonical', url()->current())
 @section('custom-content')
     <div class="my-2">
 
-        <!-- title -->
-        <h1 class="text-xl text-center mb-3">{{ $article->title }}</h1>
 
-        <!-- description -->
-        <div>
-            {!! $article->description !!}
+        <div class="flex flex-col lg:flex-row gap-3">
+            <div class="lg:w-[83%] px-3 text-justify">
+                <!--top adsterra ad-->
+                <div class="hidden md:block bg-red-400 mx-auto w-[468px] h-[60px]"></div>
+                <div class="w-[320px] h-[50px] bg-pink-400 my-2 mx-auto"></div>
+
+                <!-- title -->
+                <h1 class="text-xl text-center mb-3">{{ $article->title }}</h1>
+
+                <!-- article image -->
+                <div class="px-3 pt-1 pb-4 w-2/3 mx-auto">
+                    <img src="{{ $article->image }}" class="rounded-lg" loading="lazy" alt="{{ $article->title }}">
+                </div>
+
+                <!-- description -->
+                {!! $article->description !!}
+
+                <!-- bottom adsterra ad -->
+                <div class="w-[300px] h-[250px] bg-teal-500 my-2 mx-auto mt-2"></div>
+
+                <!-- native banner -->
+                <div class="w-full bg-blue-600 h-[150px]">Native banner</div>
+            </div>
+            <div class="lg:w-auto px-3 hidden fixed right-0 top-1/2 translate-y-[-50%] lg:block w-auto">
+                <div class="w-[160px] h-[300px] bg-lime-500 my-2"></div>
+            </div>
         </div>
     </div>
 @endsection
